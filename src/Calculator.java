4136d890586515445d29f78794a2ba1faaf8de90
lavsexpert <lavrov-sergey@yandex.ru>
2019-08-14 10:54:21 +0300

    Добавлены функции

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Calculator {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите 1е число:");
         int number1 = scanner.nextInt();
         System.out.println("Введите операцию(+,-,*,/):");
@@ -22,4 +23,29 @@ public class Calculator {
             System.out.println("Результат неизвестен из-за неизвестной операции: " + operation);
         }
     }
+
+    public static int getNumber(){
+        System.out.println("Введите число:");
+        return scanner.nextInt();
+    }
+
+    public static char getOperation(){
+        System.out.println("Введите операцию(+,-,*,/):");
+        return scanner.next().charAt(0);
+    }
+
+    public static void showResult(int number1, char operation, int number2){
+        if(operation == '+'){
+            System.out.println("Результат: " + (number1 + number2));
+        } else if (operation == '-'){
+            System.out.println("Результат: " + (number1 - number2));
+        } else if (operation == '*'){
+            System.out.println("Результат: " + (number1 * number2));
+        } else if (operation == '/'){
+            System.out.println("Результат: " + (number1 / number2));
+        } else {
+            System.out.println("Результат неизвестен из-за неизвестной операции: " + operation);
+        }
+    }
+
 }
