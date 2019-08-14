d7700cd8cdd944ffeaa078a6bb08dd6ff7305aa1
lavsexpert <lavrov-sergey@yandex.ru>
2019-08-14 11:01:30 +0300

    Вторая версия кода

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,36 +5,23 @@ public class Calculator {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args){
-        System.out.println("Введите 1е число:");
-        int number1 = scanner.nextInt();
-        System.out.println("Введите операцию(+,-,*,/):");
-        char operation = scanner.next().charAt(0);
-        System.out.println("Введите 2е число:");
-        int number2 = scanner.nextInt();
-        if(operation == '+'){
-            System.out.println("Результат: " + (number1 + number2));
-        } else if (operation == '-'){
-            System.out.println("Результат: " + (number1 - number2));
-        } else if (operation == '*'){
-            System.out.println("Результат: " + (number1 * number2));
-        } else if (operation == '/'){
-            System.out.println("Результат: " + (number1 / number2));
-        } else {
-            System.out.println("Результат неизвестен из-за неизвестной операции: " + operation);
-        }
+        int number1 = getNumber();
+        char operation = getOperation();
+        int number2 = getNumber();
+        showResult(number1, operation, number2);
     }
 
-    public static int getNumber(){
+    private static int getNumber(){
         System.out.println("Введите число:");
         return scanner.nextInt();
     }
 
-    public static char getOperation(){
+    private static char getOperation(){
         System.out.println("Введите операцию(+,-,*,/):");
         return scanner.next().charAt(0);
     }
 
-    public static void showResult(int number1, char operation, int number2){
+    private static void showResult(int number1, char operation, int number2){
         if(operation == '+'){
             System.out.println("Результат: " + (number1 + number2));
         } else if (operation == '-'){
