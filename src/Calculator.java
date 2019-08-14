bd745da5fd686b644965fa46c80756fb54a8f4d0
lavsexpert <lavrov-sergey@yandex.ru>
2019-08-14 12:22:35 +0300

    Проект разделен на модули

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,44 +1,6 @@
-import java.util.Scanner;
-
 public class Calculator {
 
-    private static Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args){
-        int number1 = getNumber();
-        char operation = getOperation();
-        int number2 = getNumber();
-        int result = getResult(number1, operation, number2);
-        System.out.println("Результат: " + result);
-    }
-
-    private static int getNumber(){
-        System.out.println("Введите число:");
-        int num;
-        if (scanner.hasNextInt()){
-            num = scanner.nextInt();
-        } else {
-            System.out.println("Ошибка при вводе числа. Попробуйте снова.");
-            scanner.next();
-            num = getNumber();
-        }
-        return num;
-    }
-
-    private static char getOperation(){
-        System.out.println("Введите операцию(+,-,*,/):");
-        char operation;
-        if (scanner.hasNext()){
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Ошибка при вводе операции. Попробуйте снова.");
-            scanner.next();
-            operation = getOperation();
-        }
-        return operation;
-    }
-
-    private static int getResult(int number1, char operation, int number2){
+    public int getResult(int number1, char operation, int number2){
         int result;
         switch (operation){
             case '+': result = number1 + number2; break;
@@ -47,7 +9,7 @@ public class Calculator {
             case '/': result = number1 / number2; break;
             default:
                 System.out.println("Операция не распознана. Попробуйте снова.");
-                result = getResult(number1, getOperation(), number2);
+                result = getResult(number1, CalculatorView.getOperation(), number2);
         }
         return result;
     }
---
+++ src/CalculatorView.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CalculatorView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int number1 = getNumber();
+        char operation = getOperation();
+        int number2 = getNumber();
+
+        Calculator calc = new Calculator();
+        int result = calc.getResult(number1, operation, number2);
+        System.out.println("Результат: " + result);
+    }
+
+    public static int getNumber(){
+        System.out.println("Введите число:");
+        int num;
+        if (scanner.hasNextInt()){
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Ошибка при вводе числа. Попробуйте снова.");
+            scanner.next();
+            num = getNumber();
+        }
+        return num;
+    }
+
+    public static char getOperation(){
+        System.out.println("Введите операцию(+,-,*,/):");
+        char operation;
+        if (scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Ошибка при вводе операции. Попробуйте снова.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+}
