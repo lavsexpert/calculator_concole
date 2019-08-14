fc0ea342fbcd13e7860b200c4f79ff3ff1e68014
lavsexpert <lavrov-sergey@yandex.ru>
2019-08-14 10:47:15 +0300

    Первая версия без разделения между функциями

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class Calculator {
+
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите 1е число:");
+        int number1 = scanner.nextInt();
+        System.out.println("Введите операцию(+,-,*,/):");
+        char operation = scanner.next().charAt(0);
+        System.out.println("Введите 2е число:");
+        int number2 = scanner.nextInt();
+        if(operation == '+'){
+            System.out.println("Результат: " + (number1 + number2));
+        } else if (operation == '-'){
+            System.out.println("Результат: " + (number1 - number2));
+        } else if (operation == '*'){
+            System.out.println("Результат: " + (number1 * number2));
+        } else if (operation == '/'){
+            System.out.println("Результат: " + (number1 / number2));
+        } else {
+            System.out.println("Результат неизвестен из-за неизвестной операции: " + operation);
+        }
     }
 }
